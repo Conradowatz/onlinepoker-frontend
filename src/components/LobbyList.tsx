a1341989b6dfe3db3ec02751d53af6aeb483fcc2
Kirstein <jk61bimu@dom.uni-leipzig.de>
2019-06-05 16:46:57 +0200

    input zeugs

--- src/components/LobbyList.tsx
+++ src/components/LobbyList.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react';
 
 import '../styles/NameInput.css';
-import {PokerClient} from "../pokerapi/PokerClient";
 
-interface  state {
+interface  State {
+    showNameInput: boolean
 }
 
-interface props {
-  api: PokerClient;
+interface Props {
 }
 
-export default class LobbyList extends React.Component<props, state> {
+export default class LobbyList extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showNameInput: false
+        }
+    }
     render() {
         return (
             <div id={'inputContainer'}>
                 <div id={'description'}>Beitrittscode: </div>
-                <input type="text"/>
+                <input onSubmit={(event) => this.handleLobbyChosen(event)} type="text"/>
                 <div id={"lobbyButtons"}>
                     <button>Lobby erstellen</button>
                     <button>Lobby beitreten</button>
                 </div>
             </div>
         )
+    }
+
+    handleLobbyChosen(event: {target: HTMLInputElement}) {
+        console.log("enter");
+        console.log(event.target.value);
     }
 }
